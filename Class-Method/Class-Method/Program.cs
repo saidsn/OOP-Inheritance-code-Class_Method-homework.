a87041aba51d715589f7820735394619e425cbd0
saidsn <saidsn@code.edu.az>
2022-06-28 23:49:39 +0400

    Homework tasks

---
+++ Class-Method/Class-Method/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Class_Method_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Factorial
+            Homework result = new Homework();
+            int n = 5;
+            int fact = Homework.Factorial(n);
+            Console.WriteLine(fact);
+            #endregion
+
+            #region Mail or password
+            MailHomework Email = new MailHomework();
+            string mail = "Cavid@code.edu.az";
+            int password = 12345;
+
+            if (MailHomework.CheckMail(mail, password))
+            {
+                Console.WriteLine("Giris olundu");
+            }
+            else
+            {
+                Console.WriteLine("Mail ve ya password sehvdir");
+            }
+            #endregion
+
+
+        }
+    }
+    #region Factorial
+    public class Homework
+    {
+        public static int Factorial(int n)
+        {
+
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+    #endregion
+
+    #region Mail or password
+    public class MailHomework
+    {
+        public static bool CheckMail(string mail, int password)
+        {
+
+            if (mail == "Cavid@code.edu.az" && password == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+    }
+    #endregion
+
+
+
+}
---
+++ OOP-Inheritance code/OOP-Inheritance/Program.cs
@@ -0,0 +1,109 @@
+﻿using Inheritance_Encapsulation.data;
+using Inheritance_Encapsulation.Models;
+using System;
+using System.Reflection;
+
+namespace Inheritance_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            //person.name = "Orxan";
+
+            // set and get with reflection for private fields
+
+            //typeof(Person).GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(person, "Akbarov");
+            //var personSurname = typeof(Person).GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(person);
+
+            //Car car = new Car();
+            //car.HorsePower = 120;
+            //Console.WriteLine(car.HorsePower);
+
+            //Animal animal = new Animal();
+
+            //Context context = new Context();
+
+            
+        }
+    }
+    class Person
+    {
+        public string name;
+        private string _surname;
+        protected int _age;
+        public string Address { get; set; }
+
+    }
+
+    class Student
+    {
+        
+    }
+
+    class Teacher:Person
+    {
+        public decimal salary;
+        public Teacher(int number)
+        {
+            
+        }
+    }
+
+    class Car
+    {
+        public string brand;
+        private int _horsepower;
+
+        public readonly int speed;
+
+        //public string color { get; private set; }
+        public Car()
+        {
+            speed = 500;
+        }
+
+        public int HorsePower 
+        {
+            get
+            {
+                if (_horsepower < 150)
+                {
+                    Console.WriteLine("can't drive");
+                    return -1;
+                }
+                return _horsepower;
+            }
+            set
+            {
+                if (value < 100)
+                {
+                    Console.WriteLine("Please set correct power");
+                    return;
+                }
+                _horsepower = value;
+            }
+        }
+    }
+}
+namespace Inheritance_Encapsulation.Models
+{
+
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+    }
+}
+namespace Inheritance_Encapsulation.data
+{
+    class Context
+    {
+        public int Id { get; set; }
+    }
+}
+
+
+    
